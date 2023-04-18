27ee98e1d22192c44f7914977945e85f4badd6d6
Elnoggernog <kililaterner@gmail.com>
2023-04-18 20:39:06 +0200

    Updated with more Colors

--- Display_DrawRect.py
+++ Display_DrawRect.py
@@ -1,8 +1,7 @@
-import time
-from picographics import PicoGraphics, DISPLAY_PICO_EXPLORER, PEN_P8 #lib für Display
+from picographics import PicoGraphics, DISPLAY_PICO_EXPLORER, PEN_RGB565 #lib für Display
 
 #Display instanzieren/initialisieren
-display = PicoGraphics(display=DISPLAY_PICO_EXPLORER) 
+display = PicoGraphics(display=DISPLAY_PICO_EXPLORER, pen_type=PEN_RGB565) 
 
 # Konstante fürs Zeichnen
 WIDTH, HEIGHT = display.get_bounds()
@@ -10,7 +9,6 @@ WIDTH, HEIGHT = display.get_bounds()
 # Farben erstellen
 BLACK = display.create_pen(0, 0, 0)
 WHITE = display.create_pen(255, 255, 255)
-GREEN = display.create_pen(0, 255, 0)
 
 # Display löschen
 display.set_pen(BLACK)
@@ -24,14 +22,14 @@ while 1:
         display.set_pen(BLACK) # Display löschen
         display.clear()
         
-        display.set_pen(GREEN) # mit Grün Zeichnen
-        display.rectangle(1,1,i,i) # Rechteck (wird immer größer)
-        display.update() 
+        display.set_pen(display.create_pen(i,100,0)) # mit verändernder Farbe zeichnen
+        display.rectangle(1,1,i,i) # Rechteck zeichnen (wird immer größer)
+        display.update() # Ausgabe aufs Display
 
     for i in range(WIDTH, 0, -2):
         display.set_pen(BLACK)
         display.clear()
         
-        display.set_pen(GREEN)
+        display.set_pen(display.create_pen(i,100,0))
         display.rectangle(1,1,i,i)
         display.update()
